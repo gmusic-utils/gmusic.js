86618f46ea0aa9c46bfdc900ec730ddc611a9dc2
Todd Wolfson <todd@twolfson.com>
2014-12-29 02:54:33 -0600

    Starting on our Google Music journey

--- test/google-music-test.js
+++ test/google-music-test.js
@@ -2,22 +2,24 @@
 var assert = require('assert');
 var browser = require('wd').remote();
 
-// Control a browser
+// Open Google Music
 browser.init({browserName: 'chrome'}, function() {
-  browser.get("http://admc.io/wd/test-pages/guinea-pig.html", function() {
+  browser.get('https://play.google.com/music/listen', function() {
     browser.title(function handleTitle (err, title) {
-      // title.should.include('WD');
       console.log(title);
-      browser.elementById('i am a link', function handleLink (err, el) {
-        browser.clickElement(el, function() {
-          /* jshint evil: true */
-          browser.eval("window.location.href", function handleEval (err, href) {
-            console.log(href);
-            // href.should.include('guinea-pig2');
-            browser.quit();
-          });
-        });
-      });
     });
+      // // title.should.include('WD');
+      // console.log(title);
+      // browser.elementById('i am a link', function handleLink (err, el) {
+      //   browser.clickElement(el, function() {
+      //     /* jshint evil: true */
+      //     browser.eval("window.location.href", function handleEval (err, href) {
+            // console.log(href);
+            // href.should.include('guinea-pig2');
+            // browser.quit();
+    //       });
+    //     });
+    //   });
+    // });
   });
 });
