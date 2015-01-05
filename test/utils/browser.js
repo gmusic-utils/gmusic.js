8adfd97ba8535bc4644fea138db27f921f798ad1
Todd Wolfson <todd@twolfson.com>
2015-01-05 07:47:08 -0600

    Moved to mocha constructs

--- test/utils/browser.js
+++ test/utils/browser.js
@@ -14,10 +14,33 @@ try {
 }
 var cookies = JSON.parse(cookiesJson);
 
+// Resolve the browser scripts
+// https://github.com/kbhomes/radiant-player-mac/blob/83f3622977f7b4b3f451422f9b025b03fb385ad6/radiant-player-mac/AppDelegate.m#L874-L894
+// TODO: Use bundled script instead of these one-offs
+var scripts = [
+  fs.readFileSync(__dirname + '/../lib/main.js', 'utf8'),
+  fs.readFileSync(__dirname + '/../lib/keyboard.js', 'utf8'),
+  fs.readFileSync(__dirname + '/../lib/mouse.js', 'utf8')
+];
+
 // Define helpers for interacting with the browser
-// Open Google Music
-browser.init({browserName: 'chrome'}, function () {
-  browser.get('https://play.google.com/music/listen', function () {
+exports.openMusic = function (options) {
+  // Fallback our options and default URL
+  options = options || {};
+  var url = options.url || 'https://play.google.com/music/listen';
+
+  // Execute many async steps
+  before(function startBrowser () {
+    this.browser = wd.remote();
+  });
+  before(function openBrowser (done) {
+    this.browser.init({browserName: 'chrome'}, done);
+  });
+  before(function navigateToMusicBeforeLogin (done) {
+    this.browser.get(url, done);
+  });
+  before(function handleLoginViaCookies (done) {
+    var browser = this.browser;
     async.forEach(cookies, function setCookies (cookie, cb) {
       // If the cookie is not for .google.com, skip it
       if (cookie.domain !== '.google.com') {
@@ -26,45 +49,26 @@ browser.init({browserName: 'chrome'}, function () {
       } else {
         browser.setCookie(cookie, cb);
       }
-    }, function handleSetCookies (err) {
-      // If there was an error, throw it
-      if (err) {
-        throw err;
-      }
-
-      // Otherwise, navigate back to Google Music
-      browser.get('https://play.google.com/music/listen', function () {
-        // Load in the scripts in order
-        // https://github.com/kbhomes/radiant-player-mac/blob/83f3622977f7b4b3f451422f9b025b03fb385ad6/radiant-player-mac/AppDelegate.m#L874-L894
-        var scripts = [
-          __dirname + '/../lib/main.js',
-          __dirname + '/../lib/keyboard.js',
-          __dirname + '/../lib/mouse.js'
-        ];
-        async.map(scripts, function loadScript (filepath, cb) {
-          fs.readFile(filepath, 'utf8', cb);
-        }, function handleScriptContents (err, scriptContents) {
-          // If there was an error, throw it
-          if (err) {
-            throw err;
-          }
-
-          // Otherwise, eval each script in order
-          async.forEachSeries(scriptContents, function evalScript (scriptContent, cb) {
-            browser.execute(scriptContent, cb);
-          }, function handleEvals (err) {
-            // If there was an error, throw it
-            if (err) {
-              throw err;
-            }
-
-            // Otherwise, continue
-            browser.title(function handleTitle (err, title) {
-              console.log(title);
-            });
-          });
-        });
-      });
-    });
+    }, done);
   });
-});
+  before(function navigateToMusicAfterLogin (done) {
+    this.browser.get(url, done);
+  });
+  before(function evalScripts (done) {
+    var browser = this.browser;
+    async.forEachSeries(scripts, function evalScript (script, cb) {
+      browser.execute(script, cb);
+    }, done);
+  });
+
+  // If we want to want to kill the session, clean it up
+  // DEV: This is useful for inspecting state of a session
+  var killBrowser = !options.dontKillBrowser;
+  if (killBrowser) {
+    after(function killBrowserFn (done) {
+      var browser = this.browser;
+      delete this.browser;
+      browser.quit(done);
+    });
+  }
+};
