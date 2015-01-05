ec7bc7c642ae0ca7950c32a26153c38608750e1d
Todd Wolfson <todd@twolfson.com>
2015-01-05 07:14:30 -0600

    Outlining tests for play/pause functionality

--- test/google-music-test.js
+++ test/google-music-test.js
@@ -1,59 +1,21 @@
-// Load in our dependencies
-var assert = require('assert');
-var fs = require('fs');
-var async = require('async');
-var browser = require('wd').remote();
+describe('A new session with Google Music', function () {
+  it('is not playing any music', function () {
 
-var cookies = require('./cookies');
+  });
 
-// Open Google Music
-browser.init({browserName: 'chrome'}, function () {
-  browser.get('https://play.google.com/music/listen', function () {
-    async.forEach(cookies, function setCookies (cookie, cb) {
-      // If the cookie is not for .google.com, skip it
-      if (cookie.domain !== '.google.com') {
-        process.nextTick(cb);
-      // Otherwise, set it
-      } else {
-        browser.setCookie(cookie, cb);
-      }
-    }, function handleSetCookies (err) {
-      // If there was an error, throw it
-      if (err) {
-        throw err;
-      }
+  describe('when we are playing music', function () {
+    it('lists the music as playing', function () {
 
-      // Otherwise, navigate back to Google Music
-      browser.get('https://play.google.com/music/listen', function () {
-        // Load in the scripts in order
-        // https://github.com/kbhomes/radiant-player-mac/blob/83f3622977f7b4b3f451422f9b025b03fb385ad6/radiant-player-mac/AppDelegate.m#L874-L894
-        var scripts = [
-          __dirname + '/../lib/main.js',
-          __dirname + '/../lib/keyboard.js',
-          __dirname + '/../lib/mouse.js'
-        ];
-        async.map(scripts, function loadScript (filepath, cb) {
-          fs.readFile(filepath, 'utf8', cb);
-        }, function handleScriptContents (err, scriptContents) {
-          // If there was an error, throw it
-          if (err) {
-            throw err;
-          }
+    });
 
-          // Otherwise, eval each script in order
-          async.forEachSeries(scriptContents, function evalScript (scriptContent, cb) {
-            browser.execute(scriptContent, cb);
-          }, function handleEvals (err) {
-            // If there was an error, throw it
-            if (err) {
-              throw err;
-            }
+    describe('and pause it', function () {
+      it('lists the music as paused', function () {
 
-            // Otherwise, continue
-            browser.title(function handleTitle (err, title) {
-              console.log(title);
-            });
-          });
+      });
+
+      describe('and when we clear the queue (aka the only way to stop)', function () {
+        it('lists the music as stopped', function () {
+
         });
       });
     });
