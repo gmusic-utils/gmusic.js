188f4575379dfe551e968ffe9fba5b5b0b2e6917
Todd Wolfson <todd@twolfson.com>
2015-01-05 07:16:05 -0600

    Added track info skeleton

---
+++ test/track-info.js
@@ -0,0 +1,12 @@
+describe('A Google Music instance playing no music', function () {
+  // TODO: We should navigate to a specific album and play the first song
+  it('has no artist/track info', function () {
+
+  });
+
+  describe('when we are playing music', function () {
+    it('has the artist/track info', function () {
+
+    });
+  });
+});
