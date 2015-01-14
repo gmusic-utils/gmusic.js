ba5a43ae6acbfdc5729d1ac99a34964e8aa42782
Todd Wolfson <todd@twolfson.com>
2015-01-13 23:02:40 -0600

    Determining if the slider has changed (seems rather complex)

--- test/play-pause-status.js
+++ test/play-pause-status.js
@@ -48,9 +48,39 @@ describe('A new session with Google Music', function () {
       });
     });
     before(function waitForPlaybackStart (done) {
-      // TODO: Wait for playback slider to move
-      //   Intentionally different from play-pause button to make sure tests are good
-      setTimeout(done, 1000);
+      // Wait for playback slider to move
+      // DEV: This is intentionally different from play-pause button which is library behavior
+      var sliderValue;
+      this.browser.waitFor(function checkSlider (browser, cb) {
+        browser.elementById('slider', function handleElement (err, el) {
+          // If there was an error, callback with it
+          if (err) {
+            return cb(err);
+          }
+
+          // Otherwise, get the slide value
+          browser.getAttribute(el, 'aria-valuenow', function handleValue (err, val) {
+            // If there was an error, callback with it
+            if (err) {
+              return cb(err);
+            }
+
+            // Otherwise, if there is a value and it's non-negative (e.g. not zero)
+            if (val && parseInt(val, 10)) {
+              // If the value has changed, return true
+              if (sliderValue !== val) {
+                return cb(null, true);
+              }
+
+              // Otherwise, save the value
+              sliderValue = val;
+            }
+
+            // Otherwise, return false
+            return cb(null, false);
+          });
+        }):
+      }, 2000, 100, done);
     });
     browserUtils.execute(function playMusic () {
       return window.playbackMode;
