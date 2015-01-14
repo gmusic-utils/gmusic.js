58d1af0410ae2d5880f381176cef3d521cfe1f73
Todd Wolfson <todd@twolfson.com>
2015-01-13 23:04:58 -0600

    Confused by this assert property complaint

--- test/play-pause-status.js
+++ test/play-pause-status.js
@@ -51,36 +51,39 @@ describe('A new session with Google Music', function () {
       // Wait for playback slider to move
       // DEV: This is intentionally different from play-pause button which is library behavior
       var sliderValue;
-      this.browser.waitFor(function checkSlider (browser, cb) {
-        browser.elementById('slider', function handleElement (err, el) {
-          // If there was an error, callback with it
-          if (err) {
-            return cb(err);
-          }
-
-          // Otherwise, get the slide value
-          browser.getAttribute(el, 'aria-valuenow', function handleValue (err, val) {
+      this.browser.waitFor({
+        assert: function checkSlider (browser, cb) {
+          browser.elementById('slider', function handleElement (err, el) {
             // If there was an error, callback with it
             if (err) {
               return cb(err);
             }
 
-            // Otherwise, if there is a value and it's non-negative (e.g. not zero)
-            if (val && parseInt(val, 10)) {
-              // If the value has changed, return true
-              if (sliderValue !== val) {
-                return cb(null, true);
+            // Otherwise, get the slide value
+            browser.getAttribute(el, 'aria-valuenow', function handleValue (err, val) {
+              // If there was an error, callback with it
+              if (err) {
+                return cb(err);
               }
 
-              // Otherwise, save the value
-              sliderValue = val;
-            }
+              // Otherwise, if there is a value and it's non-negative (e.g. not zero)
+              if (val && parseInt(val, 10)) {
+                // If the value has changed, return true
+                if (sliderValue !== val) {
+                  return cb(null, true);
+                }
 
-            // Otherwise, return false
-            return cb(null, false);
+                // Otherwise, save the value
+                sliderValue = val;
+              }
+
+              // Otherwise, return false
+              return cb(null, false);
+            });
           });
-        });
-      }, 2000, 100, done);
+        },
+        timeout: 2000
+      }, done);
     });
     browserUtils.execute(function playMusic () {
       return window.playbackMode;
