1f2460cb85ab3368f7329e868f795639ae4d30f2
Todd Wolfson <todd@twolfson.com>
2015-01-13 23:49:46 -0600

    Seeing if there is magic promise behavior by not passing callback in one place but yes in another

--- test/play-pause-status.js
+++ test/play-pause-status.js
@@ -53,28 +53,20 @@ describe('A new session with Google Music', function () {
       // DEV: This is intentionally different from play-pause button which is library behavior
       var sliderValue;
       this.browser.waitFor(new Asserter(function checkSlider (browser, cb) {
-        browser.elementById('slider', function handleElement (err, el) {
+        browser.elementById('slider').getAttribute('aria-valuenow', function handleValue (err, val) {
           // If there was an error, callback with it
           if (err) {
             return cb(err);
           }
 
-          // Otherwise, get the slide value
-          browser.getAttribute(el, 'aria-valuenow', function handleValue (err, val) {
-            // If there was an error, callback with it
-            if (err) {
-              return cb(err);
-            }
+          // Otherwise, if there is a value, it's non-negative (e.g. not zero), and it has changed, return true
+          if (val && parseInt(val, 10) && sliderValue !== val) {
+            return cb(null, true);
+          }
 
-            // Otherwise, if there is a value, it's non-negative (e.g. not zero), and it has changed, return true
-            if (val && parseInt(val, 10) && sliderValue !== val) {
-              return cb(null, true);
-            }
-
-            // Otherwise, save the value and return false
-            sliderValue = val;
-            return cb(null, false);
-          });
+          // Otherwise, save the value and return false
+          sliderValue = val;
+          return cb(null, false);
         });
       }), 2000, 100, done);
     });
