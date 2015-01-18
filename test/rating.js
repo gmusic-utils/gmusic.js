1b9b2bc2768704bf481878ad946e968c1bbf5382
Todd Wolfson <todd@twolfson.com>
2015-01-18 01:48:34 -0600

    Added thumbs down and thumbs up

--- test/rating.js
+++ test/rating.js
@@ -4,24 +4,44 @@ var browserUtils = require('./utils/browser');
 var browserMusicUtils = require('./utils/browser-music');
 
 // Start our tests
+// TODO: Test doesn't exist due to difficulty of testing
 describe.skip('An unrated track in Google Music', function () {
   it('has no rating', function () {
     // Placeholder for linter
   });
 });
 
-describe.skip('A track in Google Music', function () {
-  browserUtils.openMusic({killBrowser: false});
-
-  describe('when \'thumbs up\'-ed', function () {
-    it('has a high rating', function () {
-      // Placeholder for linter
-    });
+describe('A track in Google Music', function () {
+  // Load up 'Unknown Album - Unknown Artist'
+  browserUtils.openMusic('https://play.google.com/music/listen#/album');
+  browserUtils.execute(function playViaApi () {
+    window.MusicAPI.Playback.playPause();
   });
 
   describe('when \'thumbs down\'-ed', function () {
+    // DEV: Warning this will skip to next track
+    browserUtils.execute(function thumbsDownTrack () {
+      window.MusicAPI.Rating.toggleThumbsDown();
+    });
+    browserUtils.execute(function thumbsDownTrack () {
+      return window.MusicAPI.Rating.getRating();
+    });
+
     it('has a low rating', function () {
-      // Placeholder for linter
+      expect(this.result).to.equal(1);
+    });
+  });
+
+  describe('when \'thumbs up\'-ed', function () {
+    browserUtils.execute(function thumbsUpTrack () {
+      window.MusicAPI.Rating.toggleThumbsUp();
+    });
+    browserUtils.execute(function thumbsUpTrack () {
+      return window.MusicAPI.Rating.getRating();
+    });
+
+    it('has a high rating', function () {
+      expect(this.result).to.equal(5);
     });
   });
 });
