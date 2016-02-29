MarshallOfSound/google-music.js

Fixed broken rating tests, added browserstack as a selenium target, added a travis config file to enable CI

lib/main.js
@@ -195,11 +195,25 @@ proto.playback = {
   forward: function () { this.playback._forwardEl.click(); },
   rewind: function () { this.playback._rewindEl.click(); },
 
-  getShuffle: function () { return this.playback._shuffleEl.getAttribute('value'); },
+  getShuffle: function () {
+    var title = this.playback._shuffleEl.getAttribute('title').toLowerCase();
+    if (title.indexOf('off') !== -1) {
+      return GoogleMusic.Playback.ALL_SHUFFLE;
+    } else {
+      return GoogleMusic.Playback.NO_SHUFFLE;
+    }
+  },
   toggleShuffle: function () { this.playback._shuffleEl.click(); },
 
   getRepeat: function () {
-    return this.playback._repeatEl.getAttribute('value');
+    var title = this.playback._repeatEl.getAttribute('title').toLowerCase();
+    if (title.indexOf('repeat off') !== -1) {
+      return GoogleMusic.Playback.NO_REPEAT;
+    } else if (title.indexOf('repeating all') !== -1) {
+      return GoogleMusic.Playback.LIST_REPEAT;
+    } else {
+      return GoogleMusic.Playback.SINGLE_REPEAT;
+    }
   },
 
   toggleRepeat: function (mode) {
@@ -274,6 +288,16 @@ proto.rating = {
     if (el && !this.rating._isElSelected(el)) {
       el.click();
     }
+  },
+
+  // Reset the rating
+  resetRating: function () {
+    var selector = SELECTORS.rating.thumbSelectorFormat.replace('{rating}', this.rating.getRating());
+    var el = this.doc.querySelector(selector);
+
+    if (el && this.rating._isElSelected(el)) {
+      el.click();
+    }
   }
 };
 
@@ -333,9 +357,9 @@ proto.hooks = {
             var durationStr = that.doc.getElementById(SELECTORS.playback.sliderId).getAttribute('aria-valuemax');
             var duration = parseInt(durationStr, 10);
 
-            title = (title) ? title.innerText : 'Unknown';
-            artist = (artist) ? artist.innerText : 'Unknown';
-            album = (album) ? album.innerText : 'Unknown';
+            title = (title) ? title.textContent : 'Unknown';
+            artist = (artist) ? artist.textContent : 'Unknown';
+            album = (album) ? album.textContent : 'Unknown';
             art = (art) ? art.src : null;
 
             // The art may be a protocol-relative URL, so normalize it to HTTPS
@@ -447,7 +471,7 @@ proto.hooks = {
         //   <div id="playerSongInfo" style=""></div>
         //   <paper-icon-button icon="remove-circle-outline" data-rating="0" role="button" tabindex="0" aria-disabled="false" class="x-scope paper-icon-button-0"></paper-icon-button>
         // jscs:enable maximumLineLength
-        if (target.dataset.rating !== undefined && target.hasAttribute('aria-label') &&
+        if (target.dataset && target.dataset.rating !== undefined && target.hasAttribute('aria-label') &&
             that.rating._isElSelected(target)) {
           that.emit('change:rating', target.dataset.rating);
         }

test/rating.js
@@ -6,7 +6,8 @@ var browserMusicUtils = require('./utils/browser-music');
 // Start our tests
 describe('A track in Google Music', function () {
   browserUtils.openMusic({
-    url: 'https://play.google.com/music/listen#/album//this-is-an-album-artist/this-is-an-album'
+    url: 'https://play.google.com/music/listen#/album//this-is-an-album-artist/this-is-an-album',
+    testName: 'Rating test'
   });
   browserUtils.execute(function setupHooks () {
     window.ratingCount = 0;
@@ -16,40 +17,13 @@ describe('A track in Google Music', function () {
   });
   browserUtils.execute(function playViaApi () {
     window.googleMusic.playback.playPause();
+    window.googleMusic.playback.toggleRepeat(window.GoogleMusic.Playback.SINGLE_REPEAT);
   });
   browserMusicUtils.waitForPlaybackStart();
 
-  describe('when \'thumbs down\'-ed', function () {
-    browserUtils.execute(function resetRating () {
-      window.googleMusic.rating.toggleThumbsUp();
-    });
-    browserUtils.execute(function thumbsDownTrack () {
-      // DEV: Warning this will skip to next track
-      window.googleMusic.rating.toggleThumbsDown();
-    });
-    browserUtils.execute(function thumbsDownTrack () {
-      return window.googleMusic.rating.getRating();
-    });
-
-    it('has a low rating', function () {
-      expect(this.result).to.equal('1');
-    });
-
-    describe('a hook result', function () {
-      browserUtils.execute(function getHookResult () {
-        return window.ratingCount;
-      });
-
-      it('was triggered', function () {
-        expect(this.result).to.be.at.least(2);
-      });
-    });
-  });
-
   describe('when \'thumbs up\'-ed', function () {
     browserUtils.execute(function resetRating () {
-      // DEV: Warning this will skip to next track
-      window.googleMusic.rating.toggleThumbsDown();
+      window.googleMusic.rating.resetRating();
     });
     browserUtils.execute(function thumbsUpTrack () {
       window.googleMusic.rating.toggleThumbsUp();
@@ -97,6 +71,48 @@ describe('A track in Google Music', function () {
             expect(this.result).to.equal('5');
           });
         });
+      });
+    });
+
+    describe('when reset', function () {
+      browserUtils.execute(function resetRating () {
+        window.googleMusic.rating.resetRating();
+      });
+      browserUtils.execute(function getRating () {
+        return window.googleMusic.rating.getRating();
+      });
+
+      it('has no rating', function () {
+        expect(this.result).to.equal('0');
+      });
+    });
+  });
+
+  describe('when \'thumbs down\'-ed', function () {
+    browserUtils.execute(function resetRating () {
+      window.googleMusic.rating.resetRating();
+    });
+    browserUtils.execute(function thumbsDownTrack () {
+      // DEV: Warning this will skip to next track
+      window.googleMusic.rating.toggleThumbsDown();
+    });
+    // DEV: Wait for the next track to start
+    browserMusicUtils.waitForPlaybackStart();
+    browserUtils.execute(function thumbsDownTrack () {
+      return window.googleMusic.rating.getRating();
+    });
+
+    it('has a low rating', function () {
+      expect(this.result).to.equal('1');
+    });
+
+    describe('a hook result', function () {
+      browserUtils.execute(function getHookResult () {
+        return window.ratingCount;
+      });
+
+      it('was triggered', function () {
+        expect(this.result).to.be.at.least(2);
       });
     });
   });

gmusic-utils/gmusic.js

lib/main.js
@@ -195,11 +195,25 @@ proto.playback = {
   forward: function () { this.playback._forwardEl.click(); },
   rewind: function () { this.playback._rewindEl.click(); },
 
-  getShuffle: function () { return this.playback._shuffleEl.getAttribute('value'); },
+  getShuffle: function () {
+    var title = this.playback._shuffleEl.getAttribute('title').toLowerCase();
+    if (title.indexOf('off') !== -1) {
+      return GMusic.Playback.ALL_SHUFFLE;
+    } else {
+      return GMusic.Playback.NO_SHUFFLE;
+    }
+  },
   toggleShuffle: function () { this.playback._shuffleEl.click(); },
 
   getRepeat: function () {
-    return this.playback._repeatEl.getAttribute('value');
+    var title = this.playback._repeatEl.getAttribute('title').toLowerCase();
+    if (title.indexOf('repeat off') !== -1) {
+      return GMusic.Playback.NO_REPEAT;
+    } else if (title.indexOf('repeating all') !== -1) {
+      return GMusic.Playback.LIST_REPEAT;
+    } else {
+      return GMusic.Playback.SINGLE_REPEAT;
+    }
   },
 
   toggleRepeat: function (mode) {
@@ -274,6 +288,16 @@ proto.rating = {
     if (el && !this.rating._isElSelected(el)) {
       el.click();
     }
+  },
+
+  // Reset the rating
+  resetRating: function () {
+    var selector = SELECTORS.rating.thumbSelectorFormat.replace('{rating}', this.rating.getRating());
+    var el = this.doc.querySelector(selector);
+
+    if (el && this.rating._isElSelected(el)) {
+      el.click();
+    }
   }
 };
 
@@ -333,9 +357,9 @@ proto.hooks = {
             var durationStr = that.doc.getElementById(SELECTORS.playback.sliderId).getAttribute('aria-valuemax');
             var duration = parseInt(durationStr, 10);
 
-            title = (title) ? title.innerText : 'Unknown';
-            artist = (artist) ? artist.innerText : 'Unknown';
-            album = (album) ? album.innerText : 'Unknown';
+            title = (title) ? title.textContent : 'Unknown';
+            artist = (artist) ? artist.textContent : 'Unknown';
+            album = (album) ? album.textContent : 'Unknown';
             art = (art) ? art.src : null;
 
             // The art may be a protocol-relative URL, so normalize it to HTTPS
@@ -447,7 +471,7 @@ proto.hooks = {
         //   <div id="playerSongInfo" style=""></div>
         //   <paper-icon-button icon="remove-circle-outline" data-rating="0" role="button" tabindex="0" aria-disabled="false" class="x-scope paper-icon-button-0"></paper-icon-button>
         // jscs:enable maximumLineLength
-        if (target.dataset.rating !== undefined && target.hasAttribute('aria-label') &&
+        if (target.dataset && target.dataset.rating !== undefined && target.hasAttribute('aria-label') &&
             that.rating._isElSelected(target)) {
           that.emit('change:rating', target.dataset.rating);
         }

test/rating.js
@@ -6,6 +6,7 @@ var browserMusicUtils = require('./utils/browser-music');
 // Start our tests
 describe('A track in Google Music', function () {
   browserUtils.openMusic({
+    testName: 'Rating test',
     url: 'https://play.google.com/music/listen#/album//this-is-an-album-artist/this-is-an-album'
   });
   browserUtils.execute(function setupHooks () {
@@ -16,40 +17,13 @@ describe('A track in Google Music', function () {
   });
   browserUtils.execute(function playViaApi () {
     window.gmusic.playback.playPause();
+    window.gmusic.playback.toggleRepeat(window.GMusic.Playback.SINGLE_REPEAT);
   });
   browserMusicUtils.waitForPlaybackStart();
 
-  describe('when \'thumbs down\'-ed', function () {
-    browserUtils.execute(function resetRating () {
-      window.gmusic.rating.toggleThumbsUp();
-    });
-    browserUtils.execute(function thumbsDownTrack () {
-      // DEV: Warning this will skip to next track
-      window.gmusic.rating.toggleThumbsDown();
-    });
-    browserUtils.execute(function thumbsDownTrack () {
-      return window.gmusic.rating.getRating();
-    });
-
-    it('has a low rating', function () {
-      expect(this.result).to.equal('1');
-    });
-
-    describe('a hook result', function () {
-      browserUtils.execute(function getHookResult () {
-        return window.ratingCount;
-      });
-
-      it('was triggered', function () {
-        expect(this.result).to.be.at.least(2);
-      });
-    });
-  });
-
   describe('when \'thumbs up\'-ed', function () {
     browserUtils.execute(function resetRating () {
-      // DEV: Warning this will skip to next track
-      window.gmusic.rating.toggleThumbsDown();
+      window.gmusic.rating.resetRating();
     });
     browserUtils.execute(function thumbsUpTrack () {
       window.gmusic.rating.toggleThumbsUp();
@@ -97,6 +71,48 @@ describe('A track in Google Music', function () {
             expect(this.result).to.equal('5');
           });
         });
+      });
+    });
+
+    describe('when reset', function () {
+      browserUtils.execute(function resetRating () {
+        window.gmusic.rating.resetRating();
+      });
+      browserUtils.execute(function getRating () {
+        return window.gmusic.rating.getRating();
+      });
+
+      it('has no rating', function () {
+        expect(this.result).to.equal('0');
+      });
+    });
+  });
+
+  describe('when \'thumbs down\'-ed', function () {
+    browserUtils.execute(function resetRating () {
+      window.gmusic.rating.resetRating();
+    });
+    browserUtils.execute(function thumbsDownTrack () {
+      // DEV: Warning this will skip to next track
+      window.gmusic.rating.toggleThumbsDown();
+    });
+    // DEV: Wait for the next track to start
+    browserMusicUtils.waitForPlaybackStart();
+    browserUtils.execute(function thumbsDownTrack () {
+      return window.gmusic.rating.getRating();
+    });
+
+    it('has a low rating', function () {
+      expect(this.result).to.equal('1');
+    });
+
+    describe('a hook result', function () {
+      browserUtils.execute(function getHookResult () {
+        return window.ratingCount;
+      });
+
+      it('was triggered', function () {
+        expect(this.result).to.be.at.least(2);
       });
     });
   });