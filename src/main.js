2db74ec22e20d1cd25d23682a4bfba2b195f14e0
Samuel Attard <samuel.r.attard@gmail.com>
2016-07-27 23:10:04 +1000

    Implement multi platform loader (commonjs, AMD and browser)

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import GMusic from './gmusic';
 
-window.GMusic = GMusic;
+if (typeof global.define === 'function' && global.define.amd) {
+  global.define('jquery', [], () => GMusic);
+} else if (typeof global.module === 'object' && module.exports) {
+  module.exports = GMusic;
+} else {
+  window.GMusic = GMusic;
+}
