08e8c42913758a40547f0e1391713190c7b205a9
Samuel Attard <samuel.r.attard@gmail.com>
2016-08-05 13:46:56 +1000

    Fix module loader

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ GMusic.Track = Track;
 
 if (typeof global.define === 'function' && global.define.amd) {
   global.define('gmusic.js', [], () => GMusic);
-} else if (typeof global.module === 'object' && module.exports) {
+} else if (typeof module === 'object' && module.exports) {
   module.exports = GMusic;
 } else {
   window.GMusic = GMusic;
